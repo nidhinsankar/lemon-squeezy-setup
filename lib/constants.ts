aeed4506f6d5794b9e7c5afec753f588f26d7896
nidhinsankar <nidhinsankar1@gmail.com>
2024-08-12 00:18:12 +0530

    code refactor

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,11 @@
 export const BASIC_PLAN = {
-    plan_name:"BASIC",
-    variant_id:"478570",   
-}
+  plan_name: "BASIC",
+  variant_id: "478570",
+  description: "this is a basic plan",
+};
 
 export const PRO_PLAN = {
-    plan_name:"PRO",
-    variant_id:"478582"
-}
+  plan_name: "PRO",
+  variant_id: "478582",
+  description: "This is a pro plan",
+};
